585d273fb0b180b9ff9dfecc59bd81aa70e421f6
Thomas Wright <thomaswright1@gmail.com>
2017-03-25 22:57:29 -0500

    update demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import Component from '../../src'
 
 let Demo = React.createClass({
   render() {
     return <div>
       <h1>firebase-middleman Demo</h1>
-      <Component/>
     </div>
   }
 })
